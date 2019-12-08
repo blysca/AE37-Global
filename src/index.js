b5812d2a19cd2c7949255fd562169edbd78850e3
blysca <blysca@gmail.com>
2019-12-08 22:47:17 +0200

    init commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import "./styles.scss";
+
+document.addEventListener("DOMContentLoaded", function(event) {
+  var header = document.querySelector('.j-header'),
+    wH = window.outerHeight;
+  header.style.height = wH + 'px';
+
+});
